4a1897a67d742a7a2642a937f3d8f76072248a59
Tatsuro Murakami <99654281+tatsurou9003@users.noreply.github.com>
2026-07-26 13:42:26 +0900

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.go
@@ -0,0 +1,24 @@
+
+func topKFrequent(nums []int, k int) []int {
+	count := make(map[int]int)
+	for _, num := range nums {
+		count[num]++
+	}
+
+	buckets := make([][]int, len(nums)+1)
+
+	for num, freq := range count {
+		buckets[freq] = append(buckets[freq], num) 
+	}	
+
+	result := make([]int, 0, k)
+	for i := len(buckets) - 1; i >= 0; i-- {
+		if len(buckets[i]) > 0 {
+			result = append(result, buckets[i]...)
+		}
+		if len(result) >= k {
+			break
+		}
+	}
+	return result[:k]
+}
