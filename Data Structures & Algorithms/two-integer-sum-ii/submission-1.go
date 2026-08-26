294e70661bd3f211bd4f826f38b04849c046a60c
Tatsuro Murakami <99654281+tatsurou9003@users.noreply.github.com>
2026-08-26 22:57:51 +0900

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.go
@@ -0,0 +1,15 @@
+func twoSum(numbers []int, target int) []int {
+	l, r := 0, len(numbers) - 1
+
+	for l < r {
+		sum := numbers[l] + numbers[r]
+		if sum > target {
+			r--
+		} else if sum < target {
+			l++
+		} else {
+			return []int{l+1, r+1}
+		}
+	}
+	return []int{}
+}
