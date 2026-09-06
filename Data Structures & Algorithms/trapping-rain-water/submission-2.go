a4371c9c3ace64fcea2db26072b77038b81b499f
Tatsuro Murakami <99654281+tatsurou9003@users.noreply.github.com>
2026-09-06 12:56:11 +0900

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.go
@@ -0,0 +1,30 @@
+func trap(height []int) int {
+	if len(height) <= 2 {
+		return 0
+	}
+	
+	l, r := 0, len(height)-1
+	leftMax, rightMax := height[l], height[r]
+	res := 0
+
+	for l < r {
+		if leftMax < rightMax {
+			l++
+			leftMax = max(leftMax, height[l])
+			res += leftMax - height[l]
+		} else {
+			r--
+			rightMax = max(rightMax, height[r])
+			res += rightMax - height[r]
+		}
+	}
+	return res
+
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
